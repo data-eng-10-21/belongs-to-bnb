2010baa26afa63639d99506b7db6ed93d3fa7371
Jeff Katz <JeffreyEricKatz@gmail.com>
2020-11-13 19:03:24 -0500

    update bnb

--- src/index.py
+++ src/index.py
@@ -29,76 +29,41 @@ def display_schema(table_name):
     cursor.fetchall()
 
 def listing_name_of_highest_price():
-    cursor.execute('SELECT name FROM listings ORDER BY price DESC LIMIT 1;')
-    return cursor.fetchall()
+    pass
 
 def id_of_location_with_lowest_longitude():
-    cursor.execute('SELECT id FROM locations ORDER BY longitude LIMIT 1;')
-    return cursor.fetchall()
+    pass
 
 def greatest_occupancy_of_listing():
-    cursor.execute('SELECT MAX(occupancy) FROM listings;')
-    return cursor.fetchall()
+    pass
 
 def avg_price_of_listing():
-    cursor.execute('SELECT AVG(price) FROM listings;')
-    return cursor.fetchall()
+    pass
 
 def number_of_hosts():
-    cursor.execute('SELECT COUNT(id) FROM hosts;')
-    return cursor.fetchall()
+    pass
 
 def longitude_and_latitude_of_listing_with_highest_price():
-    cursor.execute(""" SELECT ROUND(a.longitude, 2), ROUND(a.latitude, 2)
-       FROM locations AS a JOIN
-       listings AS b ON a.id = b.location_id ORDER BY b.price DESC
- LIMIT 1; """)
-    return cursor.fetchall()
+    pass
 
 def neighborhood_id_of_lowest_price_listing():
-    cursor.execute(""" SELECT a.neighborhood_id
-       FROM locations AS a
-       JOIN listings AS b ON a.id = b.location_id
- ORDER BY b.price LIMIT 1;
-""")
-    return cursor.fetchall()
+    pass
 
 def long_lat_of_lowest_price_listing():
-    cursor.execute("""SELECT ROUND(a.longitude, 2),
-       ROUND(a.latitude, 2) FROM locations AS a
-       JOIN listings AS b ON a.id = b.location_id
- ORDER BY b.price LIMIT 1;""")
-    return cursor.fetchall()
+    pass
 
 def host_with_most_reviews():
-    cursor.execute(""" SELECT h.host_name,
-       SUM(l.number_of_reviews) FROM hosts AS h
-       JOIN listings AS l ON h.id = l.host_id
- GROUP BY l.host_id ORDER BY SUM(l.number_of_reviews) DESC
- LIMIT 1;""")
-    return cursor.fetchall()
+    pass
 
 
 def host_name_with_lowest_avg_listing_price():
-    cursor.execute("""SELECT h.host_name FROM hosts AS h
-       JOIN
-       listings AS l ON h.id = l.host_id GROUP BY l.host_id
- ORDER BY AVG(l.price) LIMIT 1; """)
-    return cursor.fetchall()
+    pass
 
 def neighborhood_with_most_locations():
-    cursor.execute("""SELECT n.name FROM neighborhoods AS n
-       JOIN
-       locations AS l ON n.id = l.neighborhood_id
- GROUP BY n.id ORDER BY COUNT(n.id) DESC LIMIT 1;""")
-    return cursor.fetchall()
+    pass
 
 def neighborhood_with_exactly_ten_locations():
-    cursor.execute("""SELECT n.name
-  FROM neighborhoods AS n JOIN
-       locations AS l ON n.id = l.neighborhood_id
- GROUP BY n.id HAVING COUNT(n.id) = 10; """)
-    return cursor.fetchall()
+    pass
     
 
     
