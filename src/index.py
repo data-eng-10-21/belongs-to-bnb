599453037019ef7091561f59b7a127c749f86c5c
JeffKatzy <jeffkatzygit config --global user.name JeffKatzy>
2021-11-23 17:53:56 -0500

    add bnb data

--- src/index.py
+++ src/index.py
@@ -2,24 +2,6 @@ import sqlite3
 conn = sqlite3.connect('airbnb.db')
 cursor = conn.cursor()
 
-import pandas as pd
-neighborhoods_url = "https://raw.githubusercontent.com/data-eng-10-21/mod-1-sql-curriculum/master/2-sql-relations/3-belongs-to-bnb/neighborhoods.csv"
-hosts_url = "https://raw.githubusercontent.com/data-eng-10-21/mod-1-sql-curriculum/master/2-sql-relations/3-belongs-to-bnb/hosts.csv"
-locations_url = "https://raw.githubusercontent.com/data-eng-10-21/mod-1-sql-curriculum/master/2-sql-relations/3-belongs-to-bnb/locations.csv"
-listings_url = "https://raw.githubusercontent.com/data-eng-10-21/mod-1-sql-curriculum/master/2-sql-relations/3-belongs-to-bnb/listings.csv"
-
-hosts_df = pd.read_csv(hosts_url)
-neighborhoods_df = pd.read_csv(neighborhoods_url)
-
-locations_df = pd.read_csv(locations_url)
-listings_df = pd.read_csv(listings_url)
-
-hosts_df.to_sql('hosts',conn, index = False, if_exists = 'replace')
-neighborhoods_df.to_sql('neighborhoods',conn, index = False, if_exists = 'replace')
-locations_df.to_sql('locations',conn, index = False, if_exists = 'replace')
-listings_df.to_sql('listings', conn, index = False, if_exists = 'replace')
-
-
 def display_tables():
     cursor.execute('SELECT name from sqlite_master where type= "table"')
     return cursor.fetchall()
